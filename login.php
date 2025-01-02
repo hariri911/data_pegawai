14f21168a36870c8a2ec9f78a361b74fdf189f70
Hariri <87254042+hariri911@users.noreply.github.com>
2025-01-02 19:33:54 +0700

    login.php

--- login.php
+++ login.php
@@ -1,21 +1,94 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-  <title>Login</title>
-  <link rel="stylesheet" type="text/css" href="file.css">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>project data pegawai hariri</title>
+
+   
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+ 
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 </head>
-<body>
 
-  <form action="proses-login.php" method="post">
-    <h2>Login</h2>
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
+<body class="bg-gradient-primary">
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
+    <div class="container">
 
-    <button type="submit">Login</button>
-  </form>
+     
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                     
+                        <div class="row">
+                       
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"><img src="img/win11img.png" align="right" width="500" height="500"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+
+                                    <div class="text-center">
+ 
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat datang di project pegawai Hariri</h1>
+                                    </div>
+                                    <form action="proses-login.php" method="post">
+                                        <div class="form-group">
+                                                <input type="text" id="username" name="username" class="form-control form-control-user" placeholder="Masukan Username" required>
+                                        </div>
+                                        <div class="form-group">
+                                                <input type="password" id="password" name="password" class="form-control form-control-user" placeholder="Masukan Password" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">ingat saya</label>
+                                            </div>
+                                        </div>
+                                        <button type="submit"  class="btn btn-primary btn-user btn-block" >Login</button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="#" onclick="LupaPassword()">Lupa Password?</a>
+
+
+                                    </div>        
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+<script>
+function LupaPassword() {
+  alert("Silahkan Kunjigi Github Hariri911 atau hubungi Hariri");
+}
+</script>
 </body>
+
 </html>
